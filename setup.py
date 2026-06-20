5d612e92f454f65f69205720823b4386e2ae7bd9
morgan <morgan.hedges@gmail.com>
2026-06-20 23:20:13 +1000

    fix streaming_data_item?

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ from setuptools import setup
 setup(
     name='DPM',    
     version='0.01', 
-    py_modules=['DPM']
+    py_modules=['DPM', 'streaming_data_item', 'DPM_listener', 'DPM_sender', 'FancyPlotItem'],
+    package_data={'': ['words']},
+    include_package_data=True
 )
